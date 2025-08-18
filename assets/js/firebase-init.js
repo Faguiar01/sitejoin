537d20d46806ea34f6b981be58b5bb5047fdc130
Faguiar01 <fabio.aguiar@edu.sc.senai.br>
2025-08-18 11:50:31 -0300

    correção 9

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,18 +1,25 @@
-// Importa as funções necessárias do Firebase
+// Importa as funções necessárias do SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
-// As suas credenciais do Firebase
+// ESTE É O BLOCO CORRETO QUE VOCÊ ENVIOU
 const firebaseConfig = {
-    apiKey: "AIzaSyAXtFYlnq57POCOp-b4TFuYucGlbn4NkAk",
-    authDomain: "joinxperience-site.firebaseapp.com",
-    projectId: "joinxperience-site",
-    storageBucket: "joinxperience-site.appspot.com",
-    messagingSenderId: "140037739806",
-    appId: "1:140037739806:web:322493d72ca6149abce50f",
-    measurementId: "G-V4SRK0FNBN"
+  apiKey: "AIzaSyAXtFYlnq57POCOp-b4TFuYucGlbn4NkAk",
+  authDomain: "joinxperience-site.firebaseapp.com",
+  projectId: "joinxperience-site",
+  storageBucket: "joinxperience-site.firebasestorage.app",
+  messagingSenderId: "140037739806",
+  appId: "1:140037739806:web:322493d72ca6149abce50f",
+  measurementId: "G-V4SRK0FNBN"
 };
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços que você vai usar em outras partes do seu site
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
